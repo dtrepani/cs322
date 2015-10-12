e2cb6b8426f13410bf3cd2798d2c448130c7935d
Fina <finapani@gmail.com>
2015-10-12 17:19:33 -0400

    PG2: Finished deep-reverse

--- PG2/pg2.rkt
+++ PG2/pg2.rkt
@@ -8,26 +8,15 @@
 
 ; NOT DONE
 (define (my-reverse a-list)
-  (if (not (pair? a-list))
-      (car a-list)
-      (list (cdr a-list) (car a-list))))
-;  (cond
-;    ((null? a-list) '())
-;    ((pair? a-list) (cons (cdr a-list) (car a-list)))
-;    (else (car a-list))))
+  (if (pair? a-list)
+      (append (my-reverse (cdr a-list)) (my-reverse (car a-list)))
+      (list a-list)))
 
-; NOT DONE
-(define (deep-reverse tree)
-  (cond
-    ((null? tree) '())
-    ((list? tree) (append (deep-reverse (cdr tree)) (deep-reverse (car tree))))
-    (else (list tree))))
-; Mostly working answer:
-;  (list (lambda (x)
-;         (if (list? x)
-;             (deep-reverse x)
-;             x))
-;       (reverse tree)))
+(define (deep-reverse a-list)
+  (if (pair? a-list)
+      (map deep-reverse a-list)
+      a-list)
+  (my-reverse a-list))
 
 (define (frontier tree)
   (if (pair? tree)
