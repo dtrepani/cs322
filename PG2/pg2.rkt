c59c5fe6be9d1db1edb9e84d45b1195b4e5390fc
Fina <finapani@gmail.com>
2015-10-12 10:30:22 -0400

    PG2: More work on my-reverse and deep-reverse.

--- PG2/pg2.rkt
+++ PG2/pg2.rkt
@@ -1,20 +1,31 @@
 #lang racket
 
 (define (pick n a-list)
-  (if (null? a-list)
-     '()
-     (if (= n 1)
-         (car a-list)
-         (pick (- n 1) (cdr a-list)))))
+  (cond
+    ((null? a-list) '())
+    ((= n 1) (car a-list))
+    (else (pick (- n 1) (cdr a-list)))))
 
 ; NOT DONE
 (define (my-reverse a-list)
-  (if (pair? a-list)
-      (list (my-reverse (cdr a-list)) (my-reverse (car a-list)))
-      (append a-list)))
+  (if (null? a-list)
+      '()
+      (if (pair? a-list)
+         (cons (cdr a-list) (car a-list))
+         (car a-list))))
 
+; NOT DONE
 (define (deep-reverse tree)
-  0)
+  (cond
+    ((null? tree) '())
+    ((list? tree) (append (deep-reverse (cdr tree)) (deep-reverse (car tree))))
+    (else (list tree))))
+; Mostly working answer:
+;  (list (lambda (x)
+;         (if (list? x)
+;             (deep-reverse x)
+;             x))
+;       (reverse tree)))
 
 (define (frontier tree)
   (if (pair? tree)
