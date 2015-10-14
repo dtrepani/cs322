0740de986227989a6f500b54ffc96c8fd717a117
Fina <finapani@gmail.com>
2015-10-13 20:04:46 -0400

    PG2: Finished my-reverse and deep-reverse.

--- PG2/pg2.rkt
+++ PG2/pg2.rkt
@@ -6,17 +6,15 @@
     ((= n 1) (car a-list))
     (else (pick (- n 1) (cdr a-list)))))
 
-; NOT DONE
 (define (my-reverse a-list)
-  (if (pair? a-list)
-      (append (my-reverse (cdr a-list)) (my-reverse (car a-list)))
-      (list a-list)))
+  (if (null? a-list)
+      '()
+      (append (my-reverse (cdr a-list)) (list (car a-list)))))
 
 (define (deep-reverse a-list)
   (if (pair? a-list)
-      (map deep-reverse a-list)
-      a-list)
-  (my-reverse a-list))
+      (my-reverse (map deep-reverse a-list))
+      a-list))
 
 (define (frontier tree)
   (if (pair? tree)
